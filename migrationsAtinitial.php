b9fb444507b0e6207082f52fd20282d3e332e158
shohan494 <shohan12224943@gmail.com>
2018-02-08 21:43:34 +0600

    foreign key migration

--- migrationsAtinitial.php
+++ migrationsAtinitial.php
@@ -2,16 +2,24 @@
 
 // use false/0/negative type value at starting point of any migration
 
-
             $table->string('verified')->default(User::UNVERIFIED_USER);
             $table->string('verification_token')->nullable();
             $table->string('admin')->default(User::REGULAR_USER);
 
-
 // or use positive as need of your project
 // 
-
 
             $table->string('verified')->default(User::VERIFIED_USER);
             $table->string('verification_token')->nullable();
             $table->string('admin')->default(User::ADMIN_USER);
+
+
+// foreign key relation ships sample
+
+            $table->integer('buyer_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+
+// naming conventions, single format + _id of table name which is plural
+
+            $table->foreign('buyer_id')->references('id')->on('users');
+            $table->foreign('product_id')->references('id')->on('products');
