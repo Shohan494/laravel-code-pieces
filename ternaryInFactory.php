8dd8692fdd6bd5b587adf943ee45fffc5951d4fc
shohan494 <shohan12224943@gmail.com>
2018-02-08 21:47:46 +0600

    ternary operator in factory

---
+++ ternaryInFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\User;
+
+$factory->define(User::class, function (Faker\Generator $faker) {
+    static $password;
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+        'verified' => $verified = $faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
+        'verification_token' => $verified == User::VERIFIED_USER ? null : User::generateVerificationCode(),
+        'verified' => $verified = $faker->randomElement([User::ADMIN_USER, User::REGULAR_USER]),
+    ];
+});
